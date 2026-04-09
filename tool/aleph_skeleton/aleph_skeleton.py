2dd94b3235b054e28d78996810696e0136d4446d
EXL <exlmotodev@gmail.com>
2026-04-10 00:39:07 +0700

    [Aleph]: Bump aleph skeleton sample

--- tool/aleph_skeleton/aleph_skeleton.py
+++ tool/aleph_skeleton/aleph_skeleton.py
@@ -3,29 +3,21 @@
 """
 Aleph Application Skeleton Template
 
-A general template for creating Python tools using Aleph library.
+A general template for creating Python tools using Aleph framework library.
 """
 
 import sys
 from pathlib import Path
-from datetime import datetime
-from argparse import Namespace
-from argparse import RawDescriptionHelpFormatter
 
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
 
-from aleph import *
+from aleph import Aleph
 
-def do_work(args: Namespace) -> bool:
-	if args.verbose:
-		I('Verbose mode enabled {check}.')
+def do_skeleton_work(a: Aleph) -> bool:
+	a.log.I(f'Working... ')
 	return True
 
-def args_and_help() -> Namespace:
-	class Args(Arguments):
-		def check_args(self, args: Namespace) -> None:
-			pass
-
+def full_skeleton_args(a: Aleph) -> None:
 	epl = 'examples:\n'
 	epl += f'  python {Path(__file__).name} -v\n'
 	hlp = {
@@ -33,19 +25,14 @@ def args_and_help() -> Namespace:
 		'v': 'Enable verbose output'
 	}
 
-	parser = Args(description=hlp['D'], epilog=epl, formatter_class=RawDescriptionHelpFormatter)
-	parser.add_argument('-v', '--verbose', action='store_true', help=hlp['v'])
-	return parser.parse_and_check_args()
+	a.args_parser.add_epilog_description(epl, hlp['D'])
+	a.args_parser.add_argument('-v', '--verbose', action='store_true', help=hlp['v'])
 
-def main() -> None:
-	args = args_and_help()
-	set_logger(args.verbose)
-
-	start_time = datetime.now()
-
-	do_work(args)
-
-	I(f'Time elapsed: "{elapsed_format(datetime.now() - start_time)}".')
+def check_skeleton_args(a: Aleph) -> None:
+	if a.args.verbose:
+		a.log.I(f'Verbose mode: {a.args.verbose}')
 
 if __name__ == '__main__':
-	main()
+	aleph = Aleph()
+	aleph.set_args(full_skeleton_args, check_skeleton_args)
+	aleph.main(do_skeleton_work)
